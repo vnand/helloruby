88c2cca4f68330ddedef4b646c345bdef7f12c06
Vidhatanand <vidhatanand@faichi.com>
2012-04-26 22:26:57 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloruby::Application.config.secret_token = '93fc3fd0b660e092b7d5586cf53f256429dc1ca30fa2f418816dfd2a7c941ebeff53536cc4ef0211fa8b97708c85e98bd3daabbaebd2908e9be08b364eba45f2'
